0524ecd540ab4a04f911ac51c5460c9a89f2f5b9
luazrk <luazrk@gmail.com>
2020-05-09 07:14:16 +0900

    atualização plugin

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,9 @@
 // This is a JavaScript file
 
+$(document).on("click", "#alerta", function(){
+  function retorno(){
+
+  }
+
+navigator.notification.alert("Minha mensagem",retorno,"Aviso!!!","Aceito");
+});
